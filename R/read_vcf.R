163e36e6bf6fba464428f6f5cf3c2a395a7ec013
Wei Jin Luo <weijinluo@gmail.com>
2023-10-24 00:12:43 -0400

    Initial commit

---
+++ R/read_vcf.R
@@ -0,0 +1,12 @@
+#' Read VCF file
+#'
+#'A function reads VCF files and extracts mutational data from each sample
+#'
+#'@param loglikelihood
+#'
+#'@param clusters
+#'
+#'@param dimension
+#'
+#'@param nObservations
+#'
